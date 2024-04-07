87b1e4dec13f44cddb03e75c523eb9ca9efeaded
[ryosuke-tokushima] <[ryosuke.tokushima@gmail.com]>
2024-04-07 19:44:16 +0900

    fix/todo-methods/tokushima

--- src/components/EditTodo.tsx
+++ src/components/EditTodo.tsx
@@ -1,60 +1,68 @@
-import { useState } from "react";
-import { ITodo } from "./Todo.type";
-import "./AddTodoForm.style.css";
+import { useEffect, useState } from 'react'
+import { ITodo } from './Todo.type'
+import './AddTodoForm.style.css'
 
 type Props = {
-  data: ITodo;
-  onBackButtonHnd : () => void 
-  onUptadeClickHnd: (data: ITodo) => void
-};
-
+  data: ITodo
+  onBackButtonHnd: () => void
+  onUpdateClickHnd: (data: ITodo) => void
+}
 
 const EditTodo = (props: Props) => {
-  const {data, onBackButtonHnd, onUptadeClickHnd} = props;
+  const { data, onBackButtonHnd, onUpdateClickHnd } = props
 
-
-  const [title, setTitle ] = useState(data.title);
-  const [description, setDescription] = useState(data.description);
+  const [title, setTitle] = useState(data.title)
+  const [description, setDescription] = useState(data.description)
 
   const onTodoTitleChangeHnd = (e: any) => {
     setTitle(e.target.value)
-  };
+  }
 
   const onDescriptionChangeHnd = (e: any) => {
     setDescription(e.target.value)
-  };
+  }
 
   const onSumitBtnClickedHnd = (e: any) => {
-    e.preventDefault();
+    e.preventDefault()
     const updateData: ITodo = {
       id: data.id,
       title: title,
       description: description,
-    };
-    onUptadeClickHnd(updateData);
-    onBackButtonHnd();
-  };
-  return (        
-  <div className="form-container">
-  <div>
-    <h3>Add Employ Form</h3>
-  </div>
-    <form onSubmit={onSumitBtnClickedHnd}>
-          <div>
-            <label>Todo Title</label>
-            <input type='text' value={title} onChange={onTodoTitleChangeHnd} />
-          </div>
-          <div>
-            <label>Description</label>
-            <input type='text' value={description} onChange={onDescriptionChangeHnd}/>
-          </div>
-          <div> 
-              <input type='button' value='Back' onClick={onBackButtonHnd}/>
-              <input type='submit' value='Update Todo'/>
-          </div>
-    </form>
-</div>
-  )
-};
+    }
+    onUpdateClickHnd(updateData)
+    onBackButtonHnd()
+  }
 
-export default EditTodo;
+  useEffect(() => {
+    setTitle(data.title)
+    setDescription(data.description)
+  }, [data])
+
+  return (
+    <div className="form-container">
+      <div>
+        <h3>Edit Todo Form</h3>
+      </div>
+      <form onSubmit={onSumitBtnClickedHnd}>
+        <div>
+          <label>Todo Title</label>
+          <input type="text" value={title} onChange={onTodoTitleChangeHnd} />
+        </div>
+        <div>
+          <label>Description</label>
+          <input
+            type="text"
+            value={description}
+            onChange={onDescriptionChangeHnd}
+          />
+        </div>
+        <div>
+          <input type="button" value="Back" onClick={onBackButtonHnd} />
+          <input type="submit" value="Update Todo" />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default EditTodo
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,84 +1,108 @@
 import React, { useEffect, useState } from 'react'
-import "./Home.style.css";
-import { ITodo, PageEnum } from './Todo.type';
-import TodoList from './TodoList';
-import AddTodo from './AddTodo';
-import EditTodo from './EditTodo';
+import './Home.style.css'
+import { v4 as uuidv4 } from 'uuid'
+import { ITodo, PageEnum } from './Todo.type'
+import TodoList from './TodoList'
+import AddTodo from './AddTodo'
+import EditTodo from './EditTodo'
 
 export default function Home() {
-  const[todoList, setTodoList] = useState([] as ITodo[]);
+  const [todoList, setTodoList] = useState<ITodo[]>(() => {
+    const ListInString = window.localStorage.getItem('TodoList')
+    return ListInString ? JSON.parse(ListInString) : []
+  })
 
-  const [shownpage, setShownPage ] = useState(PageEnum.list);
-  const [todoEdit, setTodoEdit ] = useState({} as ITodo);
+  const [shownpage, setShownPage] = useState(PageEnum.list)
+  const [todoEdit, setTodoEdit] = useState<ITodo>({
+    id: '',
+    title: '',
+    description: '',
+  }) // 初期値として、空の値を持つITodoオブジェクトを設定
 
   useEffect(() => {
-    const listInString = window.localStorage.getItem("TodoList");
-    if(listInString){
-    setTodoList(JSON.parse(listInString));
+    const listInString = window.localStorage.getItem('TodoList')
+    if (listInString) {
+      setTodoList(JSON.parse(listInString))
     }
-  }, []);
+  }, [])
 
+  useEffect(() => {
+    window.localStorage.setItem('TodoList', JSON.stringify(todoList))
+  }, [todoList])
 
-  const onAddTodoClickedHnd = () => {
-    setShownPage(PageEnum.add);
-  };
+  const setAddPage = () => {
+    // addTodoへのページ遷移の状態を保存しているのでsetAddPageに名称変更
+    setShownPage(PageEnum.add)
+  }
 
   const showListPage = () => {
     setShownPage(PageEnum.list)
-  };
-
-  const _setTodoList = (list: ITodo[]) => {
-    setTodoList(list);
-    window.localStorage.setItem("TodoList", JSON.stringify(list));
-  };
+  }
 
   const addTodoHnd = (data: ITodo) => {
-    _setTodoList([...todoList, data]);
-  };
-
-
+    const newTodo = {
+      ...data,
+      id: uuidv4(), // 一意のIDを生成
+    }
+    setTodoList([...todoList, newTodo])
+  }
 
   const deleteTodo = (data: ITodo) => {
-    const indexToDelete = todoList.indexOf(data);
-    const tempList = [...todoList];
-
-    tempList.splice(indexToDelete, 1);
-    _setTodoList(tempList)
-  };
+    const filteredTodoList = todoList.filter((todo) => todo.id !== data.id) //spliceメソッドからfilterメソッドに変更
+    setTodoList(filteredTodoList)
+  }
 
   const editTodo = (data: ITodo) => {
     setShownPage(PageEnum.edit)
-    setTodoEdit(data);
+    setTodoEdit(data)
   }
 
-  const updateData = (data: ITodo) => {
-    const filerData = todoList.filter(x => x.id === data.id)[0];
-    const indexOfRecord = todoList.indexOf(filerData);
-    const tempData = [...todoList];
-    tempData[indexOfRecord] = data;
-    _setTodoList(tempData);
+  const updateData = (updatedTodo: ITodo) => {
+    const updatedTodoList = todoList.map((todo) =>
+      todo.id === updatedTodo.id ? updatedTodo : todo
+    )
+
+    setTodoList(updatedTodoList)
   }
 
   return (
-    <><article className='article-header'>
-          <header>
-              <h1>This is Simple CRUD todo using React</h1>
-          </header>
+    <>
+      <article className="article-header">
+        <header>
+          <h1>This is Simple CRUD todo using React</h1>
+        </header>
       </article>
-      
-      <section className='section-content'>
-        {shownpage === PageEnum.list &&
-        <>  
-          <input type='button' value='Add Todo' className='add-todo-button' onClick={onAddTodoClickedHnd}/>
-          <TodoList list={todoList} onDeleteClickHnd={deleteTodo} onEditClickHnd={editTodo}/>
-        </>
-        }
+
+      <section className="section-content">
+        {shownpage === PageEnum.list && (
+          <>
+            <input
+              type="button"
+              value="Add Todo"
+              className="add-todo-button"
+              onClick={setAddPage}
+            />
+            <TodoList
+              list={todoList}
+              onDeleteClickHnd={deleteTodo}
+              onEditClickHnd={editTodo}
+            />
+          </>
+        )}
         {shownpage === PageEnum.add && (
-       <AddTodo onBackButtonHnd={showListPage} onSubmitClickHnd={addTodoHnd}/>)
-        }
-        {shownpage === PageEnum.edit && <EditTodo data={todoEdit} onBackButtonHnd={showListPage} onUptadeClickHnd={updateData} />}
+          <AddTodo
+            onBackButtonHnd={showListPage}
+            onSubmitClickHnd={addTodoHnd}
+          />
+        )}
+        {shownpage === PageEnum.edit && (
+          <EditTodo
+            data={todoEdit}
+            onBackButtonHnd={showListPage}
+            onUpdateClickHnd={updateData}
+          />
+        )}
       </section>
-      
     </>
   )
 }
